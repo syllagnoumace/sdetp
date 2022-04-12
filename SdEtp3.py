02921a9192f757b2e0611220a74dd52063c538f8
syllagnoumace <100538160+syllagnoumace@users.noreply.github.com>
2022-04-12 08:45:16 +0300

    Add files via upload

---
+++ SdEtp3.py
@@ -0,0 +1,25 @@
+myInt = 12
+myFloat =17.9
+myString ="SdE2"
+def myFunction(n):
+    n= int ("1,2,3,4,5,6,7,8,9,n")
+    print (n)
+var1 = 12
+var2 = 17.9
+var3 = 'SdE2'
+x = type(var1)
+y = type(var2)
+z = type(var3)
+print(x , var1)
+print(y , var2)
+print(z , var3)
+nombre = input('Écris un nombre entier positif : ')
+nombre = int(nombre)
+i = 2
+while i < nombre and nombre % i != 0 :
+    i = i + 1
+    i = 2
+while i < nombre and nombre % i != 0 :
+    i = i + 1
+
+
